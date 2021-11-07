385dbb52b2430f17a9f76986216788f168ca2b13
GRG <@JammerGRG>
2021-11-07 18:01:07 +0000

    Got camera working sufficiently well

--- Project/Scenes/CameraController.gd
+++ Project/Scenes/CameraController.gd
@@ -3,7 +3,9 @@ extends Camera2D
 const MAX_SPEED = 5
 
 var playerNode = null
-var velocity = Vector2(0,0)
+
+var targetPosArray
+var currPosIndex
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -12,7 +14,9 @@ func _ready():
 	var offset = get_viewport_rect().size/2
 	global_position = playerNode.global_position - offset
 	
-
+	targetPosArray = [ global_position, global_position, global_position, global_position, global_position ] 
+	currPosIndex = 0
+	
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	var offset = get_viewport_rect().size/2
@@ -21,13 +25,24 @@ func _process(delta):
 					+ playerNode.linear_velocity
 					- offset
 				)
+	targetPosArray[currPosIndex] = targetPos
+	currPosIndex = (currPosIndex+1) % len(targetPosArray)
 	
-	var movementNeeded = targetPos - global_position
-	if movementNeeded.length() > 2:
-		velocity = lerp(velocity, movementNeeded, 0.15)
-		if velocity.length() > MAX_SPEED:
-			velocity = velocity.normalized()*MAX_SPEED
-	else:
-		velocity = Vector2(0,0)
+	var meanTargetPos = Vector2(0,0)
+	for entry in targetPosArray:
+		meanTargetPos += entry
+	meanTargetPos *= 1.0/len(targetPosArray)
 
-	global_position += velocity
+	global_position = lerp(global_position, meanTargetPos, 0.75)
+	#global_position = meanTargetPos
+#	var movementNeeded = targetPos - global_position
+#	if movementNeeded.length() > 2:
+#		speed = lerp(speed, movementNeeded.length(), 0.5)
+#		if speed > MAX_SPEED:
+#			speed = MAX_SPEED
+#		movementNeeded = movementNeeded.normalized()*speed
+#	else:
+#		speed = 0
+#		movementNeeded = Vector2(0,0)
+#
+#	global_position += movementNeeded
