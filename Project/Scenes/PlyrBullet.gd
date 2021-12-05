f91294c37000a3a8065719b531559990be8d941d
GRG <@JammerGRG>
2021-12-05 14:47:16 +0000

    Added enemies and explosions

--- Project/Scenes/PlyrBullet.gd
+++ Project/Scenes/PlyrBullet.gd
@@ -2,17 +2,20 @@ extends KinematicBody2D
 
 
 # Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
+const BULLET_DAMAGE = 1
+
 const BASE_SPEED = 350
 const BASE_VEL = Vector2.UP * BASE_SPEED
 var vel
 var fired = false
 
+const LEVEL_CONTROLLER_PATH = "/root/LevelRoot/LevelController"
+var levelController = null
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
-
+	levelController = get_node(LEVEL_CONTROLLER_PATH)
+	
 func fire(gunVel):
 	fired = true
 	vel = transform.basis_xform(BASE_VEL) + gunVel
@@ -27,5 +30,7 @@ func _physics_process(delta):
 		var frameMove = vel * delta;
 		var collision = move_and_collide(frameMove);
 		if collision != null:
+			levelController.spawnSmallExplosion(get_global_transform().get_origin())
+			levelController.doDamage(collision.collider, BULLET_DAMAGE)
 			hide()
 			queue_free()
